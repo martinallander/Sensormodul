6754813620b6b61be9c18bff910fbf53b4665a48
perliljestrom <jrperka@gmail.com>
2018-04-20 11:43:00 +0200

    New branch

---
+++ ToF_test/ToF_test/ToF_test.c
@@ -0,0 +1,47 @@
+/*
+ * ToF_test.c
+ *
+ * Created: 4/20/2018 10:26:52 AM
+ *  Author: perli397
+ */ 
+
+
+#include <avr/io.h>
+
+/*=======================================================================================
+							Troligen fel tillvägagångssätt
+	Tänker flytta runt alla header-filer och så och se om det fungerar bättre.						
+	
+=======================================================================================*/
+////VL53L0X platform inc includes
+//
+//#include "VL53L0X_1.0.2/Api/platform/inc/vl53l0x_platform_log.h"
+//#include "VL53L0X_1.0.2/Api/platform/inc/vl53l0x_types.h"
+//
+////VL53L0X Core inc includes
+//#include "VL53L0X_1.0.2/Api/core/inc/vl53l0x_api.h"
+//#include "VL53L0X_1.0.2/Api/core/inc/vl53l0x_api_calibration.h"
+//#include "VL53L0X_1.0.2/Api/core/inc/vl53l0x_api_core.h"
+//#include "VL53L0X_1.0.2/Api/core/inc/vl53l0x_api_ranging.h"
+//#include "VL53L0X_1.0.2/Api/core/inc/vl53l0x_api_strings.h"
+//#include "VL53L0X_1.0.2/Api/core/inc/vl53l0x_def.h"
+//#include "VL53L0X_1.0.2/Api/core/inc/vl53l0x_device.h"
+//#include "VL53L0X_1.0.2/Api/core/inc/vl53l0x_interrupt_threshold_settings.h"
+//#include "VL53L0X_1.0.2/Api/core/inc/vl53l0x_tuning.h"
+//
+//#include "VL53L0X_1.0.2/Api/platform/inc/vl53l0x_i2c_platform.h"
+//#include "VL53L0X_1.0.2/Api/platform/inc/vl53l0x_platform.h"
+//
+////vet inte om detta fungerar. kan vara så att jag bara behöver flytta alla C-filer till
+////samma mapp som ToF_test.c
+//
+////VL53L0X Core src includes
+//#include "VL53L0X_1.0.2/Api/core/src/"
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
