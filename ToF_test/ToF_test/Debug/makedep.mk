561aa2a11c5dd541f4a9967006974871e836fa5c
perliljestrom <jrperka@gmail.com>
2018-04-20 14:46:14 +0200

    Organized ToF, not finished

--- ToF_test/ToF_test/Debug/makedep.mk
+++ ToF_test/ToF_test/Debug/makedep.mk
@@ -2,5 +2,19 @@
 # Automatically-generated file. Do not edit or delete the file
 ################################################################################
 
-ToF_test.c
+src\vl53l0x_api.c
+
+src\vl53l0x_api_calibration.c
+
+src\vl53l0x_api_core.c
+
+src\vl53l0x_api_ranging.c
+
+src\vl53l0x_api_strings.c
+
+src\vl53l0x_platform.c
+
+src\vl53l0x_platform_log.c
+
+ToF_test_main.c
 
---
+++ ToF_test/ToF_test/ToF_test_main.c
@@ -0,0 +1,27 @@
+/*
+ * ToF_test.c
+ *
+ * Created: 4/20/2018 10:26:52 AM
+ *  Author: perli397
+ */ 
+
+
+#include <avr/io.h>
+
+/*=======================================================================================
+							Troligen fel tillvägagångssätt
+	Tänker flytta runt alla header-filer och så och se om det fungerar bättre.						
+=======================================================================================*/
+
+/*=======================================================================================
+			Det är något fel med filen "vl53l0x_i2c_win_serial_comms.c"
+			Den hittar inte flera headerfiler....
+=======================================================================================*/
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
