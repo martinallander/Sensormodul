d25d62abc8a8c1d3f8357895fd87637633c044f5
perliljestrom <jrperka@gmail.com>
2018-04-25 09:11:29 +0200

    First push of branch

--- optiskavst/optiskavst/optiskavst.c
+++ optiskavst/optiskavst/optiskavst.c
@@ -6,13 +6,16 @@
  */ 
 
 
+/*************************************************************************
+		Optisk avståndsmätare GP2Y0A21YK 10 - 80 cm
+***************************************************************************/
+
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
 
-/*************************************************************************
-		Optisk avståndsmätare GP2Y0A21YK 10 - 80 cm
-***************************************************************************/
+volatile uint16_t data_out;
+
 
 /*sätt max värdet för volt-in i AREF (minus 1 LSB).
 Är det VREF?
@@ -25,37 +28,57 @@ AREF är en pin som (jag tror) maxvärdet ska kopplas...
 
 //resultatet hamnar i registerna ADCH och ADCL. s. 242
 
-//Vilket sampling-mode ska vi använda? s.242-243
 
 /*ska vi ha timead sampling? s. 243-244
 svar: vi vill sampla kontinuerligt och sedan hämta data typ var 10sek
- Är det AutoTriggering??
+ Är det AutoTriggering eller free-running mode??
 */
 
 //Hur väljer man input channels och vilka ska vi ha?
 
 //för att minska strömanvändningen se DIDR0 s.259
+/************************************************************************
+	Vi ska använda Free Running mode, genom att använda ADC interupt Flag
+	som trigger source! check!
+************************************************************************/
 
 void init_AD_conv(void)
 {
-	/*enamble vänster sorterat resultat. s. 242
-	Här väljer VReF om jag förstår rätt.
+	/*enable vänster sorterat resultat. s. 242
+	Det är i ADMUX som vi väljer VReF om jag förstår rätt.
 	*/
-	ADMUX = 1<<ADLAR;
+	//Tror inte vi ska vänsterskifta datan.
+	//ADMUX = 1<<ADLAR;
 	
-	//Man kan välja vad som är avbrottstriggern genom att sätta
-	//bitarna ADTS i ADCSRB
-	ADCSRA = 1<<ADEN | 1<<ADIE | 1<<ADPS1 | 1<<ADPS0;
+	/*
+	enable ADC, Auto Trigger och sätter Free Running mode,
+	 avbrott och sätter prescaler bits.
+	 Osäker på ADATE
+	*/
+	ADCSRA = 1<<ADEN | 1<<ADATE | 1<<ADIE | 1<<ADPS1 | 1<<ADPS0;
 	
 	//Inte samma funktionalitet i 1284P som i 16...
 	//MCUCR = 1<<SM0 | 1<<ISC01 | 1<<ISC00;
-
 }
 
 int main(void)
 {
+	init_AD_conv();
+	
     while(1)
     {
         //TODO:: Please write your application code 
     }
+}
+
+/*
+		Det som typ återstår är förstå hur Free Running mode fungerar
+*/
+ISR(ADC_vect)
+{
+	//OBS! ADCL ska läsas först sen ADCH, Görs det här???
+	data_out = (uint16_t)((ADCH << 8) | ADCL);
+	//hoppar man ur avbrottet direkt eller måste man göra detta?
+	ADCSRA = (0 << ADEN) | (0 << ADIE)
+	
 }
